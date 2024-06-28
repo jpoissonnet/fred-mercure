dc6ecf578aca179be1f1bb893a45268f0c77b6da
jules.poissonnet <jules.poissonnet@bedrockstreaming.com>
2024-06-28 11:33:41 +0200

    communictaion !!

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { useEffect, useState } from "react";
+import { Payload } from "./Dashboard.tsx";
+
+export enum GROUPS {
+  TRUMPET,
+  SAXOPHONE,
+  DRUMS,
+  BASS,
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [group, setGroup] = useState<GROUPS | null>(null);
+  const [message, setMessage] = useState<Payload | null>(null);
+
+  useEffect(() => {
+    const url = new URL("https://localhost/.well-known/mercure");
+    const group = GROUPS[
+      Math.floor((Math.random() * Object.keys(GROUPS).length) / 2)
+    ] as unknown as GROUPS;
+    setGroup(group);
+    url.searchParams.append("topic", `https://fred-mercure.com/group/${group}`); //TODO: make this dynamic
+
+    const eventSource = new EventSource(url);
+    eventSource.onmessage = (e) => {
+      const data = JSON.parse(e.data);
+      setMessage(data);
+      console.log(e);
+    };
+    return () => {
+      eventSource.close();
+    };
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>You are a musician of the group {group}</h1>
+      <p>Currently playing {message?.note ?? "nothing"}</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Dashboard.tsx
@@ -0,0 +1,55 @@
+import { GROUPS } from "./App.tsx";
+import { useState } from "react";
+
+export interface Payload {
+  note: string;
+}
+
+const sendPayload = (payload: Payload, group: keyof typeof GROUPS) =>
+  fetch("https://localhost/.well-known/mercure", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJtZXJjdXJlIjp7InB1Ymxpc2giOlsiKiJdLCJzdWJzY3JpYmUiOlsiaHR0cHM6Ly9leGFtcGxlLmNvbS9teS1wcml2YXRlLXRvcGljIiwie3NjaGVtZX06Ly97K2hvc3R9L2RlbW8vYm9va3Mve2lkfS5qc29ubGQiLCIvLndlbGwta25vd24vbWVyY3VyZS9zdWJzY3JpcHRpb25zey90b3BpY317L3N1YnNjcmliZXJ9Il0sInBheWxvYWQiOnsidXNlciI6Imh0dHBzOi8vZXhhbXBsZS5jb20vdXNlcnMvZHVuZ2xhcyIsInJlbW90ZUFkZHIiOiIxMjcuMC4wLjEifX19.KKPIikwUzRuB3DTpVw6ajzwSChwFw5omBMmMcWKiDcM",
+    },
+    body: new URLSearchParams({
+      topic: `https://fred-mercure.com/group/${group}`,
+      data: JSON.stringify(payload),
+    }),
+  });
+
+const Dashboard = () => {
+  const [inputs, setInputs] = useState<Record<keyof typeof GROUPS, Payload>>(
+    {} as Record<keyof typeof GROUPS, Payload>,
+  );
+
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      {(Object.keys(GROUPS) as Array<keyof typeof GROUPS>).map((group) => {
+        if (!isNaN(Number(group))) return null;
+        return (
+          <label key={group} htmlFor={group}>
+            <p>{group.toLowerCase()}</p>
+            <input
+              type="text"
+              id={group}
+              onChange={(e) =>
+                setInputs({
+                  ...inputs,
+                  [group]: { ...inputs[group], note: e.target.value },
+                })
+              }
+            />
+            <button onClick={() => sendPayload(inputs[group] ?? {}, group)}>
+              Send
+            </button>
+          </label>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Dashboard;
